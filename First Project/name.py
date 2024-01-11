230a872482d2b706f7bb5b220290294b776e54b1
Sujanu Giri <gireee57@gmail.com>
2024-01-11 21:30:40 +0545

    changed to storing variable

--- First Project/name.py
+++ First Project/name.py
@@ -1,4 +1,8 @@
 #input will get user input in console
-# then print() will print the word "Hello" and the user input
+# then print() will print the word "Hello" and the usernick
+
 
 print("Hello " + input (" What is your name ? "))
+
+avg = input ("enter your city : ")
+print("city is " + avg)
