6843d2804c824f98c16767efabcdc450634afd7c
Sujanu Giri <gireee57@gmail.com>
2024-01-11 22:34:49 +0545

    sum of two inputed digit

--- First Project/sum2.py
+++ First Project/sum2.py
@@ -1,1 +1,15 @@
-print(123.5+ 4)
+# sum of two inputed digit number
+
+a = input("enter two digit number ")
+
+#changing a from integer to string 
+a = str(a)
+
+#seprating 2 digits
+b = a[0]
+c = a[1]
+
+# taking sum by changing 2 seperated digits from string to intetger 
+sum = int(b) + int (c)
+
+print(sum)
