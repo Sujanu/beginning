aa30e83ee4b7732bb9b8927adb87059c4c749867
Sujanu Giri <gireee57@gmail.com>
2024-01-11 23:04:35 +0545

    body mass index of a person

---
+++ First Project/body_mass.py
@@ -0,0 +1,10 @@
+# to calculate body mass index of a person
+
+h = input("Enter height in meter: ")
+w = input("Enter weight in kg: ")
+
+#changing weight to integer and height to float // height in meter in float many times
+BMI = int(w) + float(h) ** 2
+
+print(BMI)
+#print(type(h2))
