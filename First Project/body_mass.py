dabde40582781b3078e0957b512767beeee1ce85
Sujanu Giri <gireee57@gmail.com>
2024-01-12 11:52:42 +0545

    python control flow if else

--- First Project/body_mass.py
+++ First Project/body_mass.py
@@ -4,6 +4,7 @@ h = input("Enter height in meter: ")
 w = input("Enter weight in kg: ")
 
 #changing weight to integer and height to float // height in meter in float many times
+
 BMI = int(w) / float(h) ** 2
 
 print(BMI)
--- First Project/controlflow.py
+++ First Project/controlflow.py
@@ -1,14 +1,14 @@
-# # control flow in python
+# control flow in python
 
-# # using if else 
+#  using if else 
  
-# num = int(input("Enter a number: "))
+num = int(input("Enter a number: "))
 
-# if  num % 2 == 0 :
-#     print(f"the given number {num} is even")  
-# else :
-#     print(f"the given number {num} is odd")
-    
+if  num % 2 == 0 :
+      print(f"the given number {num} is even")  
+else :
+     print(f"the given number {num} is odd")
+
 # # using if elif and else
 # # body mass index based in user's weight and height
 
@@ -17,6 +17,19 @@ print("\n \n \n \n \n ")
 height = float(input("Enter the height in meter: "))
 weight = float(input("Enter the weight in kg: " ))
 
-BMI = we
+BMI = round(weight / height ** 2)
 
-    
+if BMI < 18.5:
+     print(f"Your BMI is {BMI} , you are underweight")
+
+elif BMI < 25:
+    print(f"Your BMI is {BMI} , you have normal weight")
+
+elif BMI <30:
+    print(f"Your BMI is {BMI} , you are overweight")
+
+elif BMI <35:
+    print(f"Your BMI is {BMI} , you are obese")
+
+else :
+    print(f"Your BMI is {BMI} , you are clinically obese")
