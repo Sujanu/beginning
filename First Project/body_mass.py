611ba65a5dcbe2eca0ea1fb77632c833e8ad199c
Sujanu Giri <gireee57@gmail.com>
2024-01-12 11:21:47 +0545

    correct BMI formula

--- First Project/body_mass.py
+++ First Project/body_mass.py
@@ -4,7 +4,7 @@ h = input("Enter height in meter: ")
 w = input("Enter weight in kg: ")
 
 #changing weight to integer and height to float // height in meter in float many times
-BMI = int(w) + float(h) ** 2
+BMI = int(w) / float(h) ** 2
 
 print(BMI)
 #print(type(h2))
---
+++ First Project/controlflow.py
@@ -0,0 +1,22 @@
+# # control flow in python
+
+# # using if else 
+ 
+# num = int(input("Enter a number: "))
+
+# if  num % 2 == 0 :
+#     print(f"the given number {num} is even")  
+# else :
+#     print(f"the given number {num} is odd")
+    
+# # using if elif and else
+# # body mass index based in user's weight and height
+
+print("\n \n \n \n \n ")
+
+height = float(input("Enter the height in meter: "))
+weight = float(input("Enter the weight in kg: " ))
+
+BMI = we
+
+    
