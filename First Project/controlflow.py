bd637f0583f07d4b392f8025f218d808c8e8c03a
Sujanu Giri <gireee57@gmail.com>
2024-01-12 12:38:20 +0545

    control flow 3 programs

--- First Project/controlflow.py
+++ First Project/controlflow.py
@@ -1,7 +1,8 @@
 # control flow in python
 
-#  using if else 
- 
+# using if else 
+
+print("Odd Even NUmber Check")
 num = int(input("Enter a number: "))
 
 if  num % 2 == 0 :
@@ -10,26 +11,47 @@ else :
      print(f"the given number {num} is odd")
 
 # # using if elif and else
-# # body mass index based in user's weight and height
+#  body mass index based in user's weight and height
 
-print("\n \n \n \n \n ")
-
+print("\n \n \n")
+print("body mass index based in user's weight and height")
 height = float(input("Enter the height in meter: "))
 weight = float(input("Enter the weight in kg: " ))
 
 BMI = round(weight / height ** 2)
 
 if BMI < 18.5:
-     print(f"Your BMI is {BMI} , you are underweight")
+      print(f"Your BMI is {BMI} , you are underweight")
 
 elif BMI < 25:
-    print(f"Your BMI is {BMI} , you have normal weight")
+     print(f"Your BMI is {BMI} , you have normal weight")
 
 elif BMI <30:
-    print(f"Your BMI is {BMI} , you are overweight")
+     print(f"Your BMI is {BMI} , you are overweight")
 
 elif BMI <35:
-    print(f"Your BMI is {BMI} , you are obese")
+     print(f"Your BMI is {BMI} , you are obese")
 
 else :
-    print(f"Your BMI is {BMI} , you are clinically obese")
+ print(f"Your BMI is {BMI} , you are clinically obese")
+
+print("\n \n \n")
+
+# leap year program
+
+print(" leap year program")
+year = int(input("enter a year: "))
+
+if year % 4 ==0 :
+    if year % 100 == 0 :
+    
+        if year % 400 == 0:
+            print(f"{year} is a leap year")
+        else :
+            print(f"{year} is not a leap year")
+    
+    else:
+     print(f"{year} is a leap year")
+
+else:
+    print(f"{year} is  not a leap year")
